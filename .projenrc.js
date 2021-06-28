d7f525c5883fe144d139315b7275288fe5a415cc
kimisme9386 <kimisme9386@gmail.com>
2021-06-28 12:07:36 +0800

    chore: add .idea to gitignore

--- .projenrc.js
+++ .projenrc.js
@@ -3,13 +3,30 @@ const project = new AwsCdkTypeScriptApp({
   cdkVersion: '1.95.2',
   defaultReleaseBranch: 'main',
   name: 'lab-ecs-fargate-cd-infra',
-
-  // cdkDependencies: undefined,        /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */
-  // deps: [],                          /* Runtime dependencies of this module. */
-  // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],                       /* Build dependencies for this module. */
-  // packageName: undefined,            /* The "name" in package.json. */
-  // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
-  // release: undefined,                /* Add release management to this project. */
+  releaseWorkflow: false,
 });
+
+project.eslint.addRules({
+  'comma-dangle': [
+    'error',
+    {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'never',
+    },
+  ],
+});
+
+const common_exclude = [
+  'cdk.out',
+  'cdk.context.json',
+  'yarn-error.log',
+  'dependabot.yml',
+  '.idea',
+];
+
+project.gitignore.exclude(...common_exclude);
+
 project.synth();
