d64e7f635069fc770dee23f1bf990ef72910cebb
kimisme9386 <kimisme9386@gmail.com>
2021-07-06 20:19:59 +0800

    test: rolling update on codepipeline

--- flask.d/app.py
+++ flask.d/app.py
@@ -6,18 +6,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return 'Welcome to flask framework'
+    return 'Welcome to flask framework. Version:2'
 
 
 @app.route('/delay')
 def deploy():
     time.sleep(3)
-    return 'Delay api for test'
+    return 'Delay api for test. Version:2'
 
 
 @app.route('/hello')
 def health_check():
-    return 'ok'
+    return 'ok Version:2'
 
 
 if __name__ == "__main__":
