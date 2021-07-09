c12881b3acbed0f49150efd0852ef12594e1477e
kimisme9386 <kimisme9386@users.noreply.github.com>
2021-07-09 00:13:07 +0000

    chore(deps): upgrade dependencies
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/kimisme9386/lab-ecs-fargate-cd-infra/actions/runs/1013476361
    
    ------
    
    *Automatically created by projen via the "upgrade-dependencies" workflow*

--- src/pipeline.ts
+++ src/pipeline.ts
@@ -141,7 +141,7 @@ export class Pipeline extends cdk.Stack {
             },
             build: {
               'on-failure': 'ABORT',
-              commands: [
+              'commands': [
                 'cd ./flask.d',
                 'docker build -t $REPOSITORY_URI:latest .',
                 'docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$IMAGE_TAG',
