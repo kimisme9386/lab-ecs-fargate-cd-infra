1071c3243292805efb739ba8ac263216643972ae
kimisme9386 <kimisme9386@gmail.com>
2021-07-15 11:27:20 +0800

    feat: add post for test

--- flask.d/app.py
+++ flask.d/app.py
@@ -1,23 +1,24 @@
-from flask import Flask
-import time
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def welcome():
     return 'Welcome to flask framework. Version:2'
 
 
-@app.route('/delay')
-def deploy():
-    time.sleep(5)
-    return 'Delay api for test. Version:2'
-
-
-@app.route('/hello')
+@app.route('/hello', methods=['GET'])
 def health_check():
     return 'It\'s version:2'
+
+
+@app.route('/login', methods=['POST'])
+def result():
+    if request.form.get('data'):
+        return request.form.get('data')
+    else:
+        return jsonify({"message": "ERROR: Unauthorized"}), 401
 
 
 if __name__ == "__main__":
