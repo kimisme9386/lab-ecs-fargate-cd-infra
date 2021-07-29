206a8babd0541f310ebbd2b85a4828ec06d3cf59
kimisme9386 <kimisme9386@gmail.com>
2021-07-29 13:55:48 +0800

    chore: remove github workflow for deployment

--- .projenrc.js
+++ .projenrc.js
@@ -53,71 +53,71 @@ const common_exclude = [
 
 project.gitignore.exclude(...common_exclude);
 
-const deployWorkflow = project.github.addWorkflow('Deploy');
-deployWorkflow.on({
-  push: {
-    branches: ['main'],
-  },
-});
+// const deployWorkflow = project.github.addWorkflow('Deploy');
+// deployWorkflow.on({
+//   push: {
+//     branches: ['main'],
+//   },
+// });
 
-deployWorkflow.addJobs({
-  aws_cdk: {
-    runsOn: 'ubuntu-latest',
-    permissions: {
-      contents: 'read',
-    },
-    steps: [
-      {
-        name: 'checkout',
-        uses: 'actions/checkout@v2',
-      },
-      {
-        name: 'AWS assume role',
-        uses: 'aws-actions/configure-aws-credentials@v1',
-        with: {
-          'aws-access-key-id': '${{ env.ASSUME_AWS_ACCESS_KEY_ID }}',
-          'aws-secret-access-key': '${{ env.ASSUME_AWS_SECRET_KEY }}',
-          'aws-region': '${{ env.CDK_DEFAULT_REGION }}',
-          'role-to-assume': '${{ env.ASSUME_ROLE_NAME }}',
-          'role-duration-seconds': 1200,
-          'role-session-name': 'github-actions-assume-role',
-        },
-      },
-      {
-        name: 'install aws-cdk',
-        run: 'sudo npm i -g aws-cdk@' + project.cdkVersion,
-      },
-      {
-        name: 'install npm package',
-        run: 'yarn --frozen-lockfile',
-      },
-      {
-        name: 'update test snapshot',
-        run: 'yarn test:update',
-      },
-      {
-        name: 'build',
-        run: 'yarn build',
-      },
-      {
-        name: 'cdk bootstrap',
-        run: 'cdk bootstrap',
-      },
-      {
-        name: 'cdk deploy',
-        run: 'cdk deploy --require-approval never --all',
-      },
-    ],
-    env: {
-      AWS_DEFAULT_REGION: 'ap-northeast-1',
-      CDK_DEFAULT_REGION: 'ap-northeast-1',
-      ASSUME_AWS_ACCESS_KEY_ID: '${{ secrets.ASSUME_AWS_ACCESS_KEY_ID }}',
-      ASSUME_AWS_SECRET_KEY: '${{ secrets.ASSUME_AWS_SECRET_KEY }}',
-      ASSUME_CDK_DEFAULT_ACCOUNT: '${{ secrets.ASSUME_CDK_DEFAULT_ACCOUNT }}',
-      STAGE: '${{ secrets.STAGE }}',
-      ASSUME_ROLE_NAME: '${{ secrets.ASSUME_ROLE_NAME }}',
-    },
-  },
-});
+// deployWorkflow.addJobs({
+//   aws_cdk: {
+//     runsOn: 'ubuntu-latest',
+//     permissions: {
+//       contents: 'read',
+//     },
+//     steps: [
+//       {
+//         name: 'checkout',
+//         uses: 'actions/checkout@v2',
+//       },
+//       {
+//         name: 'AWS assume role',
+//         uses: 'aws-actions/configure-aws-credentials@v1',
+//         with: {
+//           'aws-access-key-id': '${{ env.ASSUME_AWS_ACCESS_KEY_ID }}',
+//           'aws-secret-access-key': '${{ env.ASSUME_AWS_SECRET_KEY }}',
+//           'aws-region': '${{ env.CDK_DEFAULT_REGION }}',
+//           'role-to-assume': '${{ env.ASSUME_ROLE_NAME }}',
+//           'role-duration-seconds': 1200,
+//           'role-session-name': 'github-actions-assume-role',
+//         },
+//       },
+//       {
+//         name: 'install aws-cdk',
+//         run: 'sudo npm i -g aws-cdk@' + project.cdkVersion,
+//       },
+//       {
+//         name: 'install npm package',
+//         run: 'yarn --frozen-lockfile',
+//       },
+//       {
+//         name: 'update test snapshot',
+//         run: 'yarn test:update',
+//       },
+//       {
+//         name: 'build',
+//         run: 'yarn build',
+//       },
+//       {
+//         name: 'cdk bootstrap',
+//         run: 'cdk bootstrap',
+//       },
+//       {
+//         name: 'cdk deploy',
+//         run: 'cdk deploy --require-approval never --all',
+//       },
+//     ],
+//     env: {
+//       AWS_DEFAULT_REGION: 'ap-northeast-1',
+//       CDK_DEFAULT_REGION: 'ap-northeast-1',
+//       ASSUME_AWS_ACCESS_KEY_ID: '${{ secrets.ASSUME_AWS_ACCESS_KEY_ID }}',
+//       ASSUME_AWS_SECRET_KEY: '${{ secrets.ASSUME_AWS_SECRET_KEY }}',
+//       ASSUME_CDK_DEFAULT_ACCOUNT: '${{ secrets.ASSUME_CDK_DEFAULT_ACCOUNT }}',
+//       STAGE: '${{ secrets.STAGE }}',
+//       ASSUME_ROLE_NAME: '${{ secrets.ASSUME_ROLE_NAME }}',
+//     },
+//   },
+// });
 
 project.synth();
